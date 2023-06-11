9ce069853dc4978e5b965351e0c8f927628afd6f
Jlep243 <68204977+Jlep243@users.noreply.github.com>
2023-06-11 15:24:05 -0600

    changed computerchoice to using an array instead of an if else statement.

--- java.js
+++ java.js
@@ -1,27 +1,14 @@
 console.log("Hello");
 
-let rock = 1;
-let scissors = 2;
-let paper = 3;
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     
-
-    let answer = Math.floor(Math.random()*3+1);
-   
-    if (answer <= 1)
-    {
-        console.log("rock");
-    }
-    else if(answer <= 2)
-    {
-        console.log("scissors");
-    }
-    else
-    {
-        console.log("paper");
-    }
+    
+    console.log(Math.floor(math.random()*3));
     
 }
+
+
 
 getComputerChoice();
