cd5d76fb84b384da97a20c835fadb6f5157c0d80
Jlep243 <68204977+Jlep243@users.noreply.github.com>
2023-06-10 19:48:34 -0600

    added a function for computer to decide their choice.

---
+++ java.js
@@ -0,0 +1,22 @@
+console.log("Hello");
+
+function getComputerChoice() {
+    
+
+    let answer = Math.floor(Math.random()*3);
+
+    if (answer = 1)
+    {
+        console.log("rock");
+    }
+    else if(answer = 2)
+    {
+        console.log("scissors");
+    }
+    else if(answer = 0)
+    {
+        console.log("paper");
+    }
+}
+
+getComputerChoice();
