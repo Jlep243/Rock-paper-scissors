33dd93b26349b17e3f61d2c5b31a0c41515cc807
Jlep243 <68204977+Jlep243@users.noreply.github.com>
2023-06-10 20:36:31 -0600

    got the computerchoice function to work

--- java.js
+++ java.js
@@ -1,22 +1,27 @@
 console.log("Hello");
 
+let rock = 1;
+let scissors = 2;
+let paper = 3;
+
 function getComputerChoice() {
     
 
-    let answer = Math.floor(Math.random()*3);
-
-    if (answer = 1)
+    let answer = Math.floor(Math.random()*3+1);
+   
+    if (answer <= 1)
     {
         console.log("rock");
     }
-    else if(answer = 2)
+    else if(answer <= 2)
     {
         console.log("scissors");
     }
-    else if(answer = 0)
+    else
     {
         console.log("paper");
     }
+    
 }
 
 getComputerChoice();
