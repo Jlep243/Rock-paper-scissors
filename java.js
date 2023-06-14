c15f81fe43fee42f6e528a03c62bc5f4d166f25a
Jlep243 <68204977+Jlep243@users.noreply.github.com>
2023-06-14 11:42:08 -0600

    got a little ahead of myself but got the basics working, WOOgit add java.js !

--- java.js
+++ java.js
@@ -1,23 +1,32 @@
-console.log("Hello");
 
 
-const choices = ["rock", "paper", "scissors"];
-
-
-
-function getComputerChoice() {
-
-    let a = Math.floor(Math.random()*3);
-    console.log(choices[a]);
-
+function computer() {
+    let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
+let playerSelection="rock";
+let computerChoice = computer();
 
 
-function playerSelection() {
-
-    let playerChoice = document.querySelector("#weapon");
-    console.log(playerChoice);
-
+function playRound(playerSelection, computerChoice){
+    
+    //winning conditions
+    if ((playerSelection == "rock" && computerChoice == "scissors")|| (playerSelection == "paper" && computerChoice == "rock")
+    || (playerSelection == "scissors" && computerChoice == "paper")){
+        //console.log(playerSelection, computerChoice);
+        alert("you WIN!!!!");
+    }
+    else if(playerSelection =="paper" && computerChoice == "scissors"|| playerSelection == "rock" && computerChoice == "paper"||
+    playerSelection == "scissors" && computerChoice == "rock") {
+        alert("you LOSE!!!");
+        //console.log(playerSelection, computerChoice);
+    }
+    else if(playerSelection == computerChoice){
+        alert("you tied!!");
+        //console.log(playerSelection, computerChoice);
+    }
 }
 
+//console.log(playRound(playerSelection, computerChoice));
+playRound(playerSelection, computerChoice);
