54388662ee8d19b829e986cd117b1b8495e227f9
Jlep243 <68204977+Jlep243@users.noreply.github.com>
2023-06-19 03:37:36 -0600

    adding scoring system

--- java.js
+++ java.js
@@ -1,35 +1,38 @@
 
-function computer() {
-  let choices = ["rock", "paper", "scissors"];
-  return choices[Math.floor(Math.random() * choices.length)];
-}
+
 
 
 
 function game(){
+    function computer() {
+        let choices = ["rock", "paper", "scissors"];
+        return choices[Math.floor(Math.random() * choices.length)];
+      }
     
-
-
-let computerChoice;
-let playerSelection;
-
+      let playerScore = 0;
+      let computerScore = 0;
+      let computerChoice;
+      let playerSelection;
 
 function playRound(playerSelection, computerChoice){   
+    
+    
     computerChoice = computer();
     playerSelection = window.prompt("CHOOSE YOUR WEAPON!!!");
     
-
     //winning conditions
     if ((playerSelection.toLowerCase() == "rock" && computerChoice == "scissors")|| (playerSelection.toLowerCase() == "paper" && computerChoice == "rock")
     || (playerSelection.toLowerCase() == "scissors" && computerChoice == "paper")){
         alert("player:" + playerSelection.toLowerCase() + " Computer:" + computerChoice);
-        alert("One point to player");
+        playerScore += 1;
+        alert("player : " + playerScore + "computer: " + computerScore);
     }
     //losing conditions
     else if(playerSelection.toLowerCase() =="paper" && computerChoice == "scissors"|| playerSelection.toLowerCase() == "rock" && computerChoice == "paper"||
     playerSelection.toLowerCase() == "scissors" && computerChoice == "rock") {
         alert("player:" + playerSelection.toLowerCase() + " Computer:" + computerChoice);
-        alert("One point to computer");
+        computerScore += 1;
+        alert("player : " + playerScore + "computer: " + computerScore);
     }
     //tie
     else if(playerSelection.toLowerCase() == computerChoice){
@@ -37,12 +40,23 @@ function playRound(playerSelection, computerChoice){
         alert("you tied!!"); 
     }
     
+   
+
+         
+    
+}
+playRound(playerSelection, computerChoice);
+playRound(playerSelection, computerChoice);
+playRound(playerSelection, computerChoice);
+playRound(playerSelection, computerChoice);
+playRound(playerSelection, computerChoice);
+
+
 }
 
 
 
 
-}
 
 
 
