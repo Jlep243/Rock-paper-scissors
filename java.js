18607c3c826bffee69b53e169cad2fe1255b5170
Jlep243 <68204977+Jlep243@users.noreply.github.com>
2023-06-18 18:37:55 -0600

    the player and computer can now pick a different selection after each round.

--- java.js
+++ java.js
@@ -1,33 +1,49 @@
 
-
 function computer() {
-    let choices = ["rock", "paper", "scissors"];
+  let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-//let playerSelection="rock";
-let computerChoice = computer();
+
+
+function game(){
+    
+
+
+let computerChoice;
+let playerSelection;
 
 
 function playRound(playerSelection, computerChoice){   
+    computerChoice = computer();
+    playerSelection = window.prompt("CHOOSE YOUR WEAPON!!!");
     
+
     //winning conditions
     if ((playerSelection.toLowerCase() == "rock" && computerChoice == "scissors")|| (playerSelection.toLowerCase() == "paper" && computerChoice == "rock")
     || (playerSelection.toLowerCase() == "scissors" && computerChoice == "paper")){
         alert("player:" + playerSelection.toLowerCase() + " Computer:" + computerChoice);
-        alert("you WIN!!!!");
+        alert("One point to player");
     }
+    //losing conditions
     else if(playerSelection.toLowerCase() =="paper" && computerChoice == "scissors"|| playerSelection.toLowerCase() == "rock" && computerChoice == "paper"||
     playerSelection.toLowerCase() == "scissors" && computerChoice == "rock") {
         alert("player:" + playerSelection.toLowerCase() + " Computer:" + computerChoice);
-        alert("you LOSE!!!");
+        alert("One point to computer");
     }
+    //tie
     else if(playerSelection.toLowerCase() == computerChoice){
         alert("player:" + playerSelection.toLowerCase() + " Computer:" + computerChoice);
         alert("you tied!!"); 
     }
+    
 }
 
-let playerSelection = window.prompt("CHOOSE YOUR WEAPON!!!");
-playRound(playerSelection, computerChoice);
 
+
+
+}
+
+
+
+game()
