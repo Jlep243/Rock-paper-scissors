1f5348af46bedb9fdbd9853e9a7def2a5d2f465d
Jlep243 <68204977+Jlep243@users.noreply.github.com>
2024-03-10 16:20:33 -0600

    buttons are now fully functional.

--- java.js
+++ java.js
@@ -1,79 +1,87 @@
     
     
-    let playerSelection;
-
-    document.querySelector(" #options .rock").onclick = function() 
-    {
-        playerSelection = "rock"
-        console.log(playerSelection);
-        game();
-    };
-
-    document.querySelector("#options .paper").onclick = function() 
-    {
-        playerSelection = "paper";
-        console.log(playerSelection);
-        game();
-    };
-
-    document.querySelector("#options .scissors").onclick = function() 
-    {
-        playerSelection = "scissors"
-        console.log(playerSelection);
-        game();
-    };
     
-    function game(){
+
     function computer() 
     {
         let choices = ["rock", "paper", "scissors"];
         return choices[Math.floor(Math.random() * choices.length)];
     };
     
+    document.querySelector(" #options .rock").onclick = function() 
+    {
+        const playerSelection = "rock";
+        console.log("player " + playerSelection);
+        const computerChoice = computer();
+        playRound(playerSelection, computerChoice);
+        
+    };
+
+    document.querySelector("#options .paper").onclick = function() 
+    {
+        const playerSelection = "paper";
+        console.log("player " + playerSelection);
+        const computerChoice = computer();
+        playRound(playerSelection, computerChoice);
+       
+    };
+
+    document.querySelector("#options .scissors").onclick = function() 
+    {
+        const playerSelection = "scissors";
+        console.log("player " + playerSelection);
+        const computerChoice = computer();
+        playRound(playerSelection, computerChoice);
+        
+    };
+
     let playerScore = 0;
     let computerScore = 0;
     let computerChoice = computer();
-    
     console.log("computer: " + computerChoice);
 
-function playRound(playerSelection, computerChoice){   
-    //winning conditions
-    if ((playerSelection == "rock" && computerChoice == "scissors")|| (playerSelection == "paper" && computerChoice == "rock")
-    || (playerSelection == "scissors" && computerChoice == "paper")){
-        alert("player:" + playerSelection + " Computer:" + computerChoice);
-        playerScore += 1;
-        alert("player : " + playerScore + "computer: " + computerScore);
-    }
-    //losing conditions
-    else if(playerSelection =="paper" && computerChoice == "scissors"|| playerSelection == "rock" && computerChoice == "paper"||
-    playerSelection == "scissors" && computerChoice == "rock") {
-        alert("player:" + playerSelection + " Computer:" + computerChoice);
-        computerScore += 1;
-        alert("player : " + playerScore + "computer: " + computerScore);
-    }
-    //tie
-    else if(playerSelection == computerChoice){
-        alert("player:" + playerSelection + " Computer:" + computerChoice);
-        alert("you tied!!"); 
-        playRound(playerSelection, computerChoice);
+     
+
+    function playRound(playerSelection, computerChoice){   
+        //winning conditions
+        if ((playerSelection == "rock" && computerChoice == "scissors")|| (playerSelection == "paper" && computerChoice == "rock")
+        || (playerSelection == "scissors" && computerChoice == "paper")){
+            alert("player:" + playerSelection + " Computer:" + computerChoice);
+            playerScore += 1;
+            alert("player : " + playerScore + "computer: " + computerScore);
+        }
+        //losing conditions
+        else if(playerSelection =="paper" && computerChoice == "scissors"|| playerSelection == "rock" && computerChoice == "paper"||
+        playerSelection == "scissors" && computerChoice == "rock") {
+            alert("player:" + playerSelection + " Computer:" + computerChoice);
+            computerScore += 1;
+            alert("player : " + playerScore + "computer: " + computerScore);
+        }
+        //tie
+        else if(playerSelection == computerChoice){
+            alert("player:" + playerSelection + " Computer:" + computerChoice);
+            alert("you tied!!"); 
+        };
+    
     };
 
-};
+    /*declareWinner();
 
-function declareWinner()
-{
-    if (playerScore > computerScore)
+    function declareWinner()
     {
+        if (playerScore > computerScore)
+        {
         alert("You won! " +"Player: " + playerScore + "computer: " + computerScore);
-    }
-    else if (computerScore > playerScore)
-    {
+        }
+        else if (computerScore > playerScore)
+        {
         alert("You lost! " +" Player: " + playerScore + " computer: " + computerScore);
-    }
-};
+        }
+    };*/
 
-};
 
-game();
+
+
+
 
 
